64e6320a2415e7bcd4c1ca20ce0240b5cd0f97c1
Filipe Shigueru <filipe.shigueru@yahoo.com.br>
2017-04-11 21:38:21 -0300

    * iniciando container de servicos

---
+++ src/src/ServiceContainer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SONFin;
+
+
+use Xtreamwayz\Pimple\Container;
+
+class ServiceContainer implements ServiceContainerInterface
+{
+    private $container;
+
+    /**
+     * ServiceContainer constructor.
+     */
+    public function __construct()
+    {
+        $this->container = new Container();
+    }
+
+    public function add(string $name, $service)
+    {
+        $this->container[$name] = $service;
+    }
+
+    public function addLazy(string $name, callable $callable)
+    {
+        $this->container[$name] = $this->container->factory($callable);
+    }
+
+    public function get(string $name)
+    {
+        return $this->container->get($name);
+    }
+
+    public function has(string $name)
+    {
+        return $this->container->has($name);
+    }
+}
